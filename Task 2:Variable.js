d4cf302754ce486ce6f98d939b58db3fde70f956
niwrejJerwin <jerwingaoat4@gmail.com>
2024-09-23 12:29:06 +0800

    Task 2:Variable.js

---
+++ Task 2:Variable.js
@@ -0,0 +1,9 @@
+let name = "Jerwin Gaoat";
+let whatDoYouWannaBecomeInYourLife = "software Developer";
+let gender = "Male";
+let twitterHandle = "@niwrejJerwin";
+
+console.log("Name:", Jerwin Gaoat);
+console.log("What I want to become:", software Developer );
+console.log("Gender:", Male);
+console.log("Twitter Handle:", niwrejJerwin);
