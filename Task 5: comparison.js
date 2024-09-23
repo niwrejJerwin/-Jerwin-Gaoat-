16c027848bd5cc8e86c1bdfdfcf11373536f567e
niwrejJerwin <jerwingaoat4@gmail.com>
2024-09-23 13:04:08 +0800

    Task 5: comparison.js

---
+++ Task 5: comparison.js
@@ -0,0 +1,17 @@
+
+
+// 1. Create a variable named firstFavNumb and store your favorite number.
+const firstFavNumb = 8;
+
+// 2. Create a variable named secondFavNumb and store your second favorite number.
+const secondFavNumb = 12; 
+
+// 3. Perform the checks
+console.log("Greater than:", 8 > 2); 
+console.log("Less than:", 8 < 2); 
+console.log("Greater than or equal to:", 8 >= 2); 
+console.log("Less than or equal to:", 8 <= 2); 
+console.log("Strict equality (===):", 8 === 2); 
+console.log("Loose equality (==):", 8 == 2); 
+console.log("Strict inequality (!==):", 8 !== 2); 
+console.log("Loose inequality (!=):", 8 != 2); 
