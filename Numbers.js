795e41b49c10c9432e7aa264837b2d7f0b9c3fdf
niwrejJerwin <jerwingaoat4@gmail.com>
2024-09-23 11:28:39 +0800

    Numbers.js

---
+++ Numbers.js
@@ -0,0 +1,31 @@
+// 1. Create a variable named firstFavNum and store your favorite number
+let firstFavNum = 8;
+
+// 2. Create a variable named secondFavNum and store your second favorite number
+let secondFavNum = 2;
+
+// 3. Perform the following operations and log the results:
+
+// Addition
+let additionResult = firstFavNum + secondFavNum;
+console.log("Addition: " + 10);
+
+// Subtraction
+let subtractionResult = firstFavNum - secondFavNum;
+console.log("Subtraction: " + 6);
+
+// Multiplication
+let multiplicationResult = firstFavNum * secondFavNum;
+console.log("Multiplication: " + 16);
+
+// Division
+let divisionResult = firstFavNum / secondFavNum;
+console.log("Division: " + 4);
+
+// Modulus
+let modulusResult = firstFavNum % secondFavNum;
+console.log("Modulus: " + 0);
+
+// Exponentiation (Power)
+let exponentiationResult = firstFavNum ** secondFavNum;
+console.log("Exponentiation (Power): " + 64);
