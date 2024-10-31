05ed31b03845ac540e18bb14a2e7b596dc7f8858
niwrejJerwin <jerwingaoat4@gmail.com>
2024-10-31 08:44:21 +0800

    Create classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,9 @@
+# Define student information variables
+student_name = "Jerwin Gaoat"
+subject_title = "Data Structures and Algorithms"
+class_schedule = "Monday and Friday, 10:00AM - 11:00AM"
+classroom = "CL B"
+class_instructor = "CO Pascua"
+
+# Log student information to console
+print(f"{student_name} is currently enrolled in {subject_title} with a class schedule of {class_schedule} at room {classroom}. The instructor for the subject is {class_instructor}")
